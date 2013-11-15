b06e1434dbb75a877a12fa9f2d201c45b06c3190
Osamu HASEGAWA <osamu@shinshu-u.ac.jp>
2013-11-15 15:16:16 +0900

    初期ソース

---
+++ src/main/java/jp/ac/u/shinshu/WicketApplication.java
@@ -0,0 +1,32 @@
+package jp.ac.u.shinshu;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * 
+ * @see jp.ac.u.shinshu.Start#main(String[])
+ */
+public class WicketApplication extends WebApplication
+{    	
+	/**
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
+	@Override
+	public Class<? extends WebPage> getHomePage()
+	{
+		return HomePage.class;
+	}
+
+	/**
+	 * @see org.apache.wicket.Application#init()
+	 */
+	@Override
+	public void init()
+	{
+		super.init();
+
+		// add your configuration here
+	}
+}
